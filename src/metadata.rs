f8effcd905a73e40bd40f18290d2928ba4829d1a
Simon Heath <simon.heath@gmail.com>
2026-01-27 17:27:20 +0100

    Cleanup unused crates. Refresh crates. Move from crates users (unmaintained) to uzers

--- src/metadata.rs
+++ src/metadata.rs
@@ -3,7 +3,7 @@ use std::env;
 use chrono::prelude::*;
 use indexmap::IndexMap;
 use sysinfo::{Pid, System};
-use users::{get_current_gid, get_current_uid, get_group_by_gid, get_user_by_uid};
+use uzers::{get_current_gid, get_current_uid, get_group_by_gid, get_user_by_uid};
 
 pub fn collect_starting_metadata(md: &mut IndexMap<&'static str, String>) {
     // Get command line
@@ -19,12 +19,11 @@ pub fn collect_starting_metadata(md: &mut IndexMap<&'static str, String>) {
 
     // Get system info
     let mut sys = System::new();
-    sys.refresh_memory();
-    sys.refresh_processes();
+    sys.refresh_all();
     if let Some(s) = System::host_name() {
         md.insert("node_name", s);
     }
-    md.insert("total_memory", format!("{} K", sys.total_memory()));
+    md.insert("total_memory", format!("{}", sys.total_memory()));
     md.insert("physical_cores", format!("{}", num_cpus::get_physical()));
     md.insert("virtual_cores", format!("{}", num_cpus::get()));
     if let Some(proc) = sys.process(Pid::from_u32(unsafe { libc::getpid() as u32 })) {
